7df745097d5de697036be06c2284c852036d8a2b
cesarcamp <danielsuarez8509@gmail.com>
2020-07-01 21:46:05 -0500

    The-report-challenge

--- MySQL-queries.sql
+++ MySQL-queries.sql
@@ -11,3 +11,16 @@ FROM
 WHERE
     co.continent = 'Asia'
 GROUP BY co.continent;
+
+--The Report
+
+-- Ketty gives Eve a task to generate a report containing three columns: Name, Grade and Mark. Ketty doesn't want the NAMES of those students who received a grade lower than 8. The report must be in descending order by grade -- i.e. higher grades are entered first. If there is more than one student with the same grade (8-10) assigned to them, order those particular students by their name alphabetically. Finally, if the grade is lower than 8, use "NULL" as their name and list them by their grades in descending order. If there is more than one student with the same grade (1-7) assigned to them, order those particular students by their marks in ascending order.
+
+
+SELECT
+    IF(grade < 8, NULL, name), grade, marks
+FROM
+    students
+        JOIN
+    grades ON students.marks BETWEEN min_mark AND max_mark
+ORDER BY grade DESC , name , marks;
